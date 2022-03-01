7356b6708f7c35d3c779a4b269af819006056a82
K NAGA HIMAJA <himajakeela18@gmail.com>
2022-03-01 21:05:44 +0530

    Create finalAndConst.dart

---
+++ finalAndConst.dart
@@ -0,0 +1,18 @@
+void main()
+{
+  //final keyword
+  final cityName = "Kurnool";
+  //cityName  = "Delhi";
+  final String countryName = "India";
+  
+  //const keyword
+  const double PIE = 3.14;
+  const gravity = 9.8;
+}
+
+class Circle
+{
+  final color = "Red";
+  //const PI = 3.14; gives error
+  static const PI = 3.14;
+}
