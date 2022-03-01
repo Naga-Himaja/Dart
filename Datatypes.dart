222deb0aa0a207fa0b2da3e2483957e0c5f2d686
K NAGA HIMAJA <himajakeela18@gmail.com>
2022-03-01 20:20:57 +0530

    Create Datatypes.dart

---
+++ Datatypes.dart
@@ -0,0 +1,24 @@
+void main()
+{
+  int score = 23;
+  double percent  =  23.5;
+  String name = "Hare";
+  bool isEmpty = true;
+  int hexValue = 0xEADEBAEE;
+  double exponent = 1.42e5;
+  
+  var s = 12;
+  var p = 34.6;
+  var n = "Krishna";
+  var isAlive = false;
+  
+  int w;
+  var y;
+  
+  print(score);
+  print(name);
+  
+  print(exponent);
+  print(hexValue);
+  print(y);
+}
