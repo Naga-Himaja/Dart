078897740f3883b006964d468d1cbfb234fde9d4
K NAGA HIMAJA <himajakeela18@gmail.com>
2022-03-02 21:10:20 +0530

    Create loops.dart

---
+++ loops.dart
@@ -0,0 +1,21 @@
+void main()
+{
+  for (var i =0;i<4;i++)
+  {
+    print("Hello");
+  }
+  
+  int i=0;
+  while(i<4)
+  {
+    print("Hello!");
+    i++;
+  }
+  
+  int j=0;
+  do
+  {
+    print("Om");
+    j++;
+  }while(j<4);
+}
