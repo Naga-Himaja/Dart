d0e4d2802007f00f3877e2ae537c063dfbf9bbc0
K NAGA HIMAJA <himajakeela18@gmail.com>
2022-03-02 20:45:15 +0530

    Create ifElseLadder.dart

---
+++ ifElseLadder.dart
@@ -0,0 +1,24 @@
+void main()
+{
+  String grade = 'A';
+  if(grade == 'A')
+  {
+    print("Very good!");
+  }
+  else if(grade=='B')
+  {
+    print("Good!");
+  }
+  else if(grade=='C')
+  {
+    print("Average!");
+  }
+  else if(grade=='D' || grade=='F')
+  {
+    print("Fail");
+  }
+  else
+  {
+    print("Invalid grade!");
+  }
+}
